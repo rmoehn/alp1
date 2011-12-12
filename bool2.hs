e0b82a80efedf7a8d93e912aa90729fcbeee3644
Richard Möhn <richard.moehn@fu-berlin.de>
2011-12-12 18:02:59 +0100

    Multischönfinkeln funktioniert!

--- bool2.hs
+++ bool2.hs
@@ -1,20 +1,25 @@
 {-# OPTIONS_GHC -XFlexibleInstances #-}
 
+-- Typklasse für Boole'sche Funktionen verlangt...
 class BoolFunk a where
-    stellenzahl :: a -> Int
+    -- Stellenzahl der Funktion
+    stellenzahl  :: a -> Int
+    -- Variante der Funktion, die auf eine Liste von Parameter angewandt
+    -- werden kann
+    multiuncurry :: a -> ([Bool] -> Bool)
 
+-- Bool-Werte als Instanzen von BoolFunk
 instance BoolFunk Bool where
-    stellenzahl bool = 0
+    stellenzahl  bool    = 0
+    multiuncurry bool [] = bool
 
+-- beliebige Boole'sche Funktion als Instanz von BoolFunk
 instance (BoolFunk a) => BoolFunk (Bool -> a) where
+    -- Funktion Argument für Argument auswerten bis Bool übrigbleibt
     stellenzahl func = (stellenzahl (func True)) + 1
+    -- Argumente aus der Liste schrittweise zuführen
+    multiuncurry f (b:bs) = multiuncurry (f b) bs
 
 testfunc :: Bool -> Bool -> Bool -> Bool
 testfunc = (\x y z -> x && y || z)
 
---apply func boollist = apply' (stellenzahl func) func boollist
-
-apply' :: BoolFunk bf => Int -> (Bool -> bf) -> [Bool] -> (Bool -> bf)
-apply' n func (b:bs)
-    | n == 0    = func
-    | otherwise = apply' (n-1) (func b) bs
