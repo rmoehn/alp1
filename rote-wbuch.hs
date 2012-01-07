d39159762be425dd768e0835fcc46fc42d6874ac
Richard Möhn <richard.moehn@fu-berlin.de>
2012-01-07 14:58:35 +0100

    Sortiersachen aus rote-wbuch.hs entfernt

--- rote-wbuch.hs
+++ rote-wbuch.hs
@@ -1,7 +1,7 @@
 --import SpeicherFunk
 --import SpeicherList
 --import SpeicherSuchbaum ...
-import SpeicherAlg
+import SpeicherList
 
 w:: WBUCH Int String
 w = leer
@@ -10,24 +10,3 @@ w1 = einf w 3 "xx"
 w2 = einf w1 5 "gg"
 w3 = entf w2 3
 w4 = einf w3 5 "neu"
-
-qsort:: Ord a=>[a]->[a]
-
-qsort [] = []
-qsort [a] = [a]
-qsort (a:rest) =
-  qsort [x | x<-rest, x<a] ++ [a] ++
-  qsort [x | x<-rest, x>=a]
-
-l1 = [6,43,2,4,2,6,7,56,9,3,2,9]
-
-msort:: Ord a=>[a]->[a]
-
-msort [] = []
-msort [a] = [a]
-msort l = verschmelze (msort m1) (msortm m2)
-  where m1 = take n' l
-        m2 = drop n' l
-        n' = div n 2
-
-
