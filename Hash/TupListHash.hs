23b3077c0cfb7100fa30b98abfbb39c5c95d9c05
Richard Möhn <richard.moehn@fu-berlin.de>
2012-01-30 19:34:24 +0100

    Hashsache ergänzt und verschoben

---
+++ Hash/TupListHash.hs
@@ -0,0 +1,36 @@
+-- stellt Funktionen bereit, um mit Hash-ähnlichen Listen zu arbeiten:
+-- [(Schlüssel, Wert)]
+module Hash.TupListHash (
+    value,
+    exists,
+)
+where
+
+-- Funktion emptyhash: gibt einen leeren Hash zurück
+emptyhash :: [(a, b)]
+emptyhash = []
+
+
+-- Funktion value: gibt den Wert zu einem bestimmten Schlüssel zurück
+value :: Eq a => [(a, b)] -> a -> b
+value [] _         = error "(Hash) Schlüssel existiert nicht."
+value (x:xs) key
+    | key == fst x = snd x
+    | otherwise    = value xs key
+
+-- Funktion exists: testet, ob der Schlüssel im gegebenen Pseudohash vorhanden
+-- ist
+exists :: Eq a => [(a, b)] -> a -> Bool
+exists [] _        = False
+exists (x:xs) key
+    | key == fst x = True
+    | otherwise    = exists xs key
+
+-- Funktion ins: fügt ein Schlüssel-Wert-Paar in einen Hash ein
+ins :: Eq a => [(a, b)] -> a -> b -> [(a, b)]
+ins hash k v = (k, v) : (delete hash k)
+
+-- Funktion delete: entfernt alle Schlüssel-Wert-Paare mit dem angegebenen
+-- Schlüssel aus einem Hash
+delete :: Eq a => [(a, b)] -> a -> [(a, b)]
+delete hash key = [(k, v) | (k, v) <- hash, k /= key]
