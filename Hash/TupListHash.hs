3a49ed98e495295532c50b34e766df2e2896b699
Richard Möhn <richard.moehn@fu-berlin.de>
2012-02-05 11:15:03 +0100

    Tupellistenhash ergänzt

--- Hash/TupListHash.hs
+++ Hash/TupListHash.hs
@@ -6,13 +6,15 @@ module Hash.TupListHash (
 )
 where
 
+type Hash a b = [(a, b)]
+
 -- Funktion emptyhash: gibt einen leeren Hash zurück
-emptyhash :: [(a, b)]
+emptyhash :: Hash a b
 emptyhash = []
 
 
 -- Funktion value: gibt den Wert zu einem bestimmten Schlüssel zurück
-value :: Eq a => [(a, b)] -> a -> b
+value :: Eq a => Hash a b -> a -> b
 value [] _         = error "(Hash) Schlüssel existiert nicht."
 value (x:xs) key
     | key == fst x = snd x
@@ -20,17 +22,17 @@ value (x:xs) key
 
 -- Funktion exists: testet, ob der Schlüssel im gegebenen Pseudohash vorhanden
 -- ist
-exists :: Eq a => [(a, b)] -> a -> Bool
+exists :: Eq a => Hash a b -> a -> Bool
 exists [] _        = False
 exists (x:xs) key
     | key == fst x = True
     | otherwise    = exists xs key
 
 -- Funktion ins: fügt ein Schlüssel-Wert-Paar in einen Hash ein
-ins :: Eq a => [(a, b)] -> a -> b -> [(a, b)]
+ins :: Eq a => Hash a b -> a -> b -> Hash a b
 ins hash k v = (k, v) : (delete hash k)
 
 -- Funktion delete: entfernt alle Schlüssel-Wert-Paare mit dem angegebenen
 -- Schlüssel aus einem Hash
-delete :: Eq a => [(a, b)] -> a -> [(a, b)]
+delete :: Eq a => Hash a b -> a -> Hash a b
 delete hash key = [(k, v) | (k, v) <- hash, k /= key]
