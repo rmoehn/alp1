49a215159f4cef29785dabe145f472be38f86926
Richard Möhn <richard.moehn@fu-berlin.de>
2012-02-06 17:30:36 +0100

    Modul für binäre zahlen nun eingeordnet

---
+++ Num/Bin.hs
@@ -0,0 +1,26 @@
+-- Typesynonyme als Instanznamen erlauben
+{-# OPTIONS_GHC -XTypeSynonymInstances #-}
+
+-- Modul zur Bereitstellung binärer Zahlen
+module Num.Bin (
+    Bin,
+    pad_bin,
+) where
+
+type Bin = [Int]
+
+-- Binärzahlen als Instanz von Enum. -- Binärzahlen werden als Liste von 0 und
+-- 1 im Big-Endian-Format aufgefasst.
+instance Enum Bin where
+    fromEnum [] = 0
+    fromEnum (x:xs) = x * 2^(length xs) + fromEnum xs
+    toEnum y = reverse (deztobin y)
+        where
+        -- wandelt Dezimalzahl in Little-Endian-Binärzahl um
+        deztobin :: Int -> Bin
+        deztobin 0 = []
+        deztobin x = mod x 2 : deztobin (div x 2)
+
+-- Funktion pad_bin: bringt eine Binärzahl auf die vorgegebene Länge
+pad_bin :: Int -> Bin -> Bin
+pad_bin lnth binnum = replicate (lnth - length binnum) 0 ++ binnum
